c29000f19ceb96f497639f0e5123395cd751265b
OndragKlaus <OndragKlaus@users.noreply.github.com>
2017-12-10 19:49:20 +0100

    implemented gpu_handler

---
+++ src/util/gpu_handler.py
@@ -0,0 +1,38 @@
+import torch
+
+from util.logger import get_logger
+
+log = get_logger(__file__)
+
+_hostname_to_gpu_id = {
+    'eec': 1,
+    'hpccremers6': 1
+}
+
+_gpu_id_default_value = -1
+
+
+def select_gpu_by_id(gpu_id: int = _gpu_id_default_value) -> None:
+    torch.cuda.set_device(device=gpu_id)
+
+
+def select_gpu_by_hostname(hostname: str) -> None:
+    gpu_id = _hostname_to_gpu_id.get(hostname, _gpu_id_default_value)
+    select_gpu_by_id(gpu_id)
+
+
+def cast_cuda_if_possible(net: torch.nn.Module, verbose: bool = False) -> torch.nn.Module:
+    if torch.cuda.is_available():
+        if verbose:
+            log.info('Using cuda')
+        return net.cuda()
+    else:
+        if verbose:
+            log.warn('Not using cuda')
+        return net
+
+
+if __name__ == '__main__':
+    select_gpu_by_id(0)
+    select_gpu_by_hostname('_')
+    select_gpu_by_hostname('hpccremers6')
