d611c47eadbc135d815e529d096f65bf0b5e9854
OndragKlaus <OndragKlaus@users.noreply.github.com>
2017-12-31 19:48:48 +0100

    refactored online training into its own method

--- src/train_online.py
+++ src/train_online.py
@@ -61,154 +61,180 @@ p = {
 
 parentModelName = 'src'
 
-# Network definition
-net = vo.OSVOS(pretrained=0)
-net.load_state_dict(torch.load(os.path.join(save_dir, parentModelName + '_epoch-' + str(parentEpoch - 1) + '.pth'),
-                               map_location=lambda storage, loc: storage))
 
-# Logging into Tensorboard
-log_dir = os.path.join(save_dir, 'runs',
-                       datetime.now().strftime('%b%d_%H-%M-%S') + '_' + socket.gethostname() + '-' + seq_name)
-writer = SummaryWriter(log_dir=log_dir)
-# y = net.forward(Variable(torch.randn(1, 3, 480, 854)))
-# writer.add_graph(net, y[-1])
+# parentModelName = 'OSVOS'
+# parentModelName = 'blackswan'
+# parentEpoch = 1000
 
-net = gpu_handler.cast_cuda_if_possible(net, verbose=True)
+def train(seq_name, nEpochs, train_and_test=True):
+    if train_and_test:
+        # db_test = db.DAVIS2016(mode='test', db_root_dir=db_root_dir, transform=tr.ToTensor(), seq_name=seq_name)
+        # testloaderx = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
+        # testitest = next(iter(testloaderx))
+        # testitest_i, testitest_g = testitest['image'], testitest['gt']
+        # testitest_i, testitest_g = Variable(testitest_i), Variable(testitest_g)
+        # testitest_i, testitest_g = gpu_handler.cast_cuda_if_possible([testitest_i, testitest_g])
 
-# Visualize the network
-if vis_net:
-    x = torch.randn(1, 3, 480, 854)
-    x = Variable(x)
-    x = gpu_handler.cast_cuda_if_possible(x)
-    y = net.forward(x)
-    g = viz.make_dot(y, net.state_dict())
-    g.view()
+        # Network definition
+        net = vo.OSVOS(pretrained=0)
+        net.load_state_dict(
+            torch.load(os.path.join(save_dir, parentModelName + '_epoch-' + str(parentEpoch - 1) + '.pth'),
+                       map_location=lambda storage, loc: storage))
+        net = gpu_handler.cast_cuda_if_possible(net, verbose=True)
 
-# Use the following optimizer
-lr = 1e-8
-wd = 0.0002
-optimizer = optim.SGD([
-    {'params': [pr[1] for pr in net.stages.named_parameters() if 'weight' in pr[0]], 'weight_decay': wd},
-    {'params': [pr[1] for pr in net.stages.named_parameters() if 'bias' in pr[0]], 'lr': lr * 2},
-    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'weight' in pr[0]], 'weight_decay': wd},
-    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'bias' in pr[0]], 'lr': lr * 2},
-    {'params': [pr[1] for pr in net.upscale.named_parameters() if 'weight' in pr[0]], 'lr': 0},
-    {'params': [pr[1] for pr in net.upscale_.named_parameters() if 'weight' in pr[0]], 'lr': 0},
-    {'params': net.fuse.weight, 'lr': lr / 100, 'weight_decay': wd},
-    {'params': net.fuse.bias, 'lr': 2 * lr / 100},
-], lr=lr, momentum=0.9)
+        # Logging into Tensorboard
+        log_dir = os.path.join(save_dir, 'runs',
+                               datetime.now().strftime('%b%d_%H-%M-%S') + '_' + socket.gethostname() + '-' + seq_name)
+        writer = SummaryWriter(log_dir=log_dir)
+        # y = net.forward(Variable(torch.randn(1, 3, 480, 854)))
+        # writer.add_graph(net, y[-1])
 
-# Preparation of the data loaders
-# Define augmentation transformations as a composition
-composed_transforms = transforms.Compose([tr.RandomHorizontalFlip(),
-                                          tr.Resize(),
-                                          # tr.ScaleNRotate(rots=(-30, 30), scales=(.75, 1.25)),
-                                          tr.ToTensor()])
-# Training dataset and its iterator
-db_train = db.DAVIS2016(mode='train', db_root_dir=db_root_dir, transform=composed_transforms, seq_name=seq_name)
-trainloader = DataLoader(db_train, batch_size=p['trainBatch'], shuffle=True, num_workers=1)
+        # Visualize the network
+        if vis_net:
+            x = torch.randn(1, 3, 480, 854)
+            x = Variable(x)
+            x = gpu_handler.cast_cuda_if_possible(x)
+            y = net.forward(x)
+            g = viz.make_dot(y, net.state_dict())
+            g.view()
 
-# Testing dataset and its iterator
-db_test = db.DAVIS2016(mode='test', db_root_dir=db_root_dir, transform=tr.ToTensor(), seq_name=seq_name)
-testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
+        # Use the following optimizer
+        lr = 1e-8
+        wd = 0.0002
+        optimizer = optim.SGD([
+            {'params': [pr[1] for pr in net.stages.named_parameters() if 'weight' in pr[0]], 'weight_decay': wd},
+            {'params': [pr[1] for pr in net.stages.named_parameters() if 'bias' in pr[0]], 'lr': lr * 2},
+            {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'weight' in pr[0]], 'weight_decay': wd},
+            {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'bias' in pr[0]], 'lr': lr * 2},
+            {'params': [pr[1] for pr in net.upscale.named_parameters() if 'weight' in pr[0]], 'lr': 0},
+            {'params': [pr[1] for pr in net.upscale_.named_parameters() if 'weight' in pr[0]], 'lr': 0},
+            {'params': net.fuse.weight, 'lr': lr / 100, 'weight_decay': wd},
+            {'params': net.fuse.bias, 'lr': 2 * lr / 100},
+        ], lr=lr, momentum=0.9)
 
-num_img_tr = len(trainloader)
-num_img_ts = len(testloader)
-loss_tr = []
-aveGrad = 0
+        # Preparation of the data loaders
+        # Define augmentation transformations as a composition
+        composed_transforms = transforms.Compose([tr.RandomHorizontalFlip(),
+                                                  tr.Resize(),
+                                                  # tr.ScaleNRotate(rots=(-30, 30), scales=(.75, 1.25)),
+                                                  tr.ToTensor()])
+        # Training dataset and its iterator
+        db_train = db.DAVIS2016(mode='train', db_root_dir=db_root_dir, transform=composed_transforms, seq_name=seq_name)
+        trainloader = DataLoader(db_train, batch_size=p['trainBatch'], shuffle=True, num_workers=1)
 
-log.info("Start of Online Training, sequence: " + seq_name)
-start_time = timeit.default_timer()
-# Main Training and Testing Loop
-for epoch in range(start_epoch, nEpochs):
-    # One training epoch
-    running_loss_tr = 0
-    for ii, sample_batched in enumerate(trainloader):
+        # Testing dataset and its iterator
+        db_test = db.DAVIS2016(mode='test', db_root_dir=db_root_dir, transform=tr.ToTensor(), seq_name=seq_name)
+        testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
 
-        inputs, gts = sample_batched['image'], sample_batched['gt']
+        num_img_tr = len(trainloader)
+        loss_tr = []
+        aveGrad = 0
 
-        # Forward-Backward of the mini-batch
-        inputs, gts = Variable(inputs), Variable(gts)
+        log.info("Start of Online Training, sequence: " + seq_name)
+        # Main Training and Testing Loop
+        for epoch in range(start_epoch, nEpochs):
+            # One training epoch
+            running_loss_tr = 0
+            for ii, sample_batched in enumerate(trainloader):
+
+                inputs, gts = sample_batched['image'], sample_batched['gt']
+
+                # Forward-Backward of the mini-batch
+                inputs, gts = Variable(inputs), Variable(gts)
+                inputs, gts = gpu_handler.cast_cuda_if_possible([inputs, gts])
+
+                outputs = net.forward(inputs)
+
+                # Compute the fuse loss
+                loss = class_balanced_cross_entropy_loss(outputs[-1], gts, size_average=False)
+                running_loss_tr += loss.data[0]
+
+                # Print stuff
+                if epoch % (nEpochs // 20) == (nEpochs // 20 - 1):
+                    running_loss_tr /= num_img_tr
+                    loss_tr.append(running_loss_tr)
+
+                    log.info('[Epoch {0}: {1}, numImages: {2}]'.format(seq_name, epoch + 1, ii + 1))
+                    log.info('Loss {0}: {1}'.format(seq_name, running_loss_tr))
+                    writer.add_scalar('data/total_loss_epoch', running_loss_tr, epoch)
+
+                # Backward the averaged gradient
+                loss /= nAveGrad
+                loss.backward()
+                aveGrad += 1
+
+                # Update the weights once in nAveGrad forward passes
+                if aveGrad % nAveGrad == 0:
+                    writer.add_scalar('data/total_loss_iter', loss.data[0], ii + num_img_tr * epoch)
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    aveGrad = 0
+
+            # Save the model
+            if (epoch % snapshot) == snapshot - 1:  # and epoch != 0:
+                torch.save(net.state_dict(), os.path.join(save_dir, seq_name + '_epoch-' + str(epoch) + '.pth'))
+
+            epoch_stop_time = timeit.default_timer()
+            # outputs = net.forward(testitest_i)
+            # loss = class_balanced_cross_entropy_loss(outputs[-1], testitest_g, size_average=False)
+            # log.info('Testitest: {}'.format(str(loss)))
+
+        # Testing Phase
+        if vis_res:
+            import matplotlib.pyplot as plt
+
+            plt.close("all")
+            plt.ion()
+            f, ax_arr = plt.subplots(1, 3)
+    else:
+        net = vo.OSVOS(pretrained=0)
+        # nEpochs = 10000
+        net.load_state_dict(torch.load(os.path.join(save_dir, 'boat' + '_epoch-' + str(nEpochs - 1) + '.pth'),
+                                       map_location=lambda storage, loc: storage))
+        net = gpu_handler.cast_cuda_if_possible(net, verbose=True)
+        db_test = db.DAVIS2016(mode='test', db_root_dir=db_root_dir, transform=tr.ToTensor(), seq_name=seq_name)
+        testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
+
+    # save_dir_res = os.path.join('results', seq_name + '_' + str(nEpochs))
+    save_dir_res = os.path.join('results', seq_name)
+    if not os.path.exists(save_dir_res):
+        os.makedirs(save_dir_res)
+
+    log.info('Testing Network')
+    # Main Testing Loop
+    for ii, sample_batched in enumerate(testloader):
+
+        img, gt, fname = sample_batched['image'], sample_batched['gt'], sample_batched['fname']
+
+        # Forward of the mini-batch
+        inputs, gts = Variable(img, volatile=True), Variable(gt, volatile=True)
         inputs, gts = gpu_handler.cast_cuda_if_possible([inputs, gts])
 
         outputs = net.forward(inputs)
 
-        # Compute the fuse loss
-        loss = class_balanced_cross_entropy_loss(outputs[-1], gts, size_average=False)
-        running_loss_tr += loss.data[0]
+        for jj in range(int(inputs.size()[0])):
+            pred = np.transpose(outputs[-1].cpu().data.numpy()[jj, :, :, :], (1, 2, 0))
+            pred = 1 / (1 + np.exp(-pred))
+            pred = np.squeeze(pred)
 
-        # Print stuff
-        if epoch % (nEpochs // 20) == (nEpochs // 20 - 1):
-            running_loss_tr /= num_img_tr
-            loss_tr.append(running_loss_tr)
+            # Save the result, attention to the index jj
+            sm.imsave(os.path.join(save_dir_res, os.path.basename(fname[jj]) + '.png'), pred)
 
-            log.info('[Epoch: %d, numImages: %5d]' % (epoch + 1, ii + 1))
-            log.info('Loss: %f' % running_loss_tr)
-            writer.add_scalar('data/total_loss_epoch', running_loss_tr, epoch)
+            if vis_res:
+                img_ = np.transpose(img.numpy()[jj, :, :, :], (1, 2, 0))
+                gt_ = np.transpose(gt.numpy()[jj, :, :, :], (1, 2, 0))
+                gt_ = np.squeeze(gt)
+                # Plot the particular example
+                ax_arr[0].cla()
+                ax_arr[1].cla()
+                ax_arr[2].cla()
+                ax_arr[0].set_title('Input Image')
+                ax_arr[1].set_title('Ground Truth')
+                ax_arr[2].set_title('Detection')
+                ax_arr[0].imshow(im_normalize(img_))
+                ax_arr[1].imshow(gt_)
+                ax_arr[2].imshow(im_normalize(pred))
+                plt.pause(0.001)
 
-        # Backward the averaged gradient
-        loss /= nAveGrad
-        loss.backward()
-        aveGrad += 1
 
-        # Update the weights once in nAveGrad forward passes
-        if aveGrad % nAveGrad == 0:
-            writer.add_scalar('data/total_loss_iter', loss.data[0], ii + num_img_tr * epoch)
-            optimizer.step()
-            optimizer.zero_grad()
-            aveGrad = 0
-
-    # Save the model
-    if (epoch % snapshot) == snapshot - 1:  # and epoch != 0:
-        torch.save(net.state_dict(), os.path.join(save_dir, seq_name + '_epoch-' + str(epoch) + '.pth'))
-
-stop_time = timeit.default_timer()
-log.info('Online training time: ' + str(stop_time - start_time))
-
-# Testing Phase
-if vis_res:
-    import matplotlib.pyplot as plt
-
-    plt.close("all")
-    plt.ion()
-    f, ax_arr = plt.subplots(1, 3)
-
-save_dir_res = os.path.join('results', seq_name)
-if not os.path.exists(save_dir_res):
-    os.makedirs(save_dir_res)
-
-log.info('Testing Network')
-# Main Testing Loop
-for ii, sample_batched in enumerate(testloader):
-
-    img, gt, fname = sample_batched['image'], sample_batched['gt'], sample_batched['fname']
-
-    # Forward of the mini-batch
-    inputs, gts = Variable(img, volatile=True), Variable(gt, volatile=True)
-    inputs, gts = gpu_handler.cast_cuda_if_possible([inputs, gts])
-
-    outputs = net.forward(inputs)
-
-    for jj in range(int(inputs.size()[0])):
-        pred = np.transpose(outputs[-1].cpu().data.numpy()[jj, :, :, :], (1, 2, 0))
-        pred = 1 / (1 + np.exp(-pred))
-        pred = np.squeeze(pred)
-
-        # Save the result, attention to the index jj
-        sm.imsave(os.path.join(save_dir_res, os.path.basename(fname[jj]) + '.png'), pred)
-
-        if vis_res:
-            img_ = np.transpose(img.numpy()[jj, :, :, :], (1, 2, 0))
-            gt_ = np.transpose(gt.numpy()[jj, :, :, :], (1, 2, 0))
-            gt_ = np.squeeze(gt)
-            # Plot the particular example
-            ax_arr[0].cla()
-            ax_arr[1].cla()
-            ax_arr[2].cla()
-            ax_arr[0].set_title('Input Image')
-            ax_arr[1].set_title('Ground Truth')
-            ax_arr[2].set_title('Detection')
-            ax_arr[0].imshow(im_normalize(img_))
-            ax_arr[1].imshow(gt_)
-            ax_arr[2].imshow(im_normalize(pred))
-            plt.pause(0.001)
+train('boat', nEpochs, train_and_test=False)
