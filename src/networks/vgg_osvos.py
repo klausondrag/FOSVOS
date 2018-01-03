58d4795330798b17ffba9ce2fab2d0c49a4a2259
OndragKlaus <OndragKlaus@users.noreply.github.com>
2018-01-03 23:46:51 +0100

    replaced copied vgg code with imports from model zoo

--- src/networks/vgg_osvos.py
+++ src/networks/vgg_osvos.py
@@ -7,6 +7,7 @@ import scipy.io
 import torch
 import torch.nn as nn
 import torch.nn.modules as modules
+from torchvision.models import vgg16
 
 from config.mypath import Path
 from layers.osvos_layers import center_crop, interp_surgery
@@ -99,14 +100,7 @@ class OSVOS(nn.Module):
 
     def load_from_pytorch(self) -> None:
         log.info('Loading weights from PyTorch VGG')
-        vgg_structure = [64, 64, 'M', 128, 128, 'M', 256, 256, 256,
-                         'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
-        _vgg = VGG(make_layers(vgg_structure))
-
-        # Load the weights from saved model
-        _vgg.load_state_dict(torch.load(os.path.join(Path.models_dir(), 'vgg16-397923af.pth'),
-                                        map_location=lambda storage, loc: storage))
-        # _vgg.load_state_dict(model_zoo.load_url('https://download.pytorch.org/models/vgg16-397923af.pth'))
+        _vgg = vgg16(pretrained=True)
 
         inds = find_conv_layers(_vgg)
         k = 0
@@ -150,58 +144,5 @@ def make_layers_osvos(cfg, in_channels):
         else:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
             layers.extend([conv2d, nn.ReLU(inplace=True)])
-            in_channels = v
-    return nn.Sequential(*layers)
-
-
-class VGG(nn.Module):
-    def __init__(self, features, num_classes=1000):
-        super(VGG, self).__init__()
-        self.features = features
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
-        self._initialize_weights()
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-            elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
-                m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
-
-
-def make_layers(cfg, batch_norm=False):
-    layers = []
-    in_channels = 3
-    for v in cfg:
-        if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
-            else:
-                layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = v
     return nn.Sequential(*layers)
