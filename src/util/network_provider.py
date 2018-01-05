ce5cb93a07236317c586c4d4fedf714f978bb1bb
OndragKlaus <OndragKlaus@users.noreply.github.com>
2018-01-05 17:49:28 +0100

    refactored init network into separate method

--- src/util/network_provider.py
+++ src/util/network_provider.py
@@ -10,12 +10,14 @@ log = get_logger(__file__)
 
 class NetworkProvider:
 
-    def __init__(self, name: str, network_type: type, save_dir: Path, **kwargs) -> None:
+    def __init__(self, name: str, network_type: type, save_dir: Path) -> None:
         self.name = name
         self.network_type = network_type
         self.save_dir = save_dir
-        self.arguments = kwargs
         self.network = None
+
+    def init_network(self, **kwargs) -> None:
+        self.network = self.network_type(**kwargs)
 
     def _get_file_path(self, epoch: int) -> str:
         return str(self.save_dir / '{0}_epoch-{1}.pth'.format(self.name, str(epoch - 1)))
@@ -23,16 +25,25 @@ class NetworkProvider:
     def load(self, epoch: int) -> None:
         file_path = self._get_file_path(epoch)
         log.info("Updating weights from: {0}".format(file_path))
-        net = self.network_type(**self.arguments)
         net.load_state_dict(torch.load(file_path, map_location=lambda storage, loc: storage))
         self.network = net
 
-# save_dir = Path('models')
-# np = NetworkProvider('vgg16', OSVOS_VGG, save_dir)
-# modelName = str(exp_name)
-# if resume_epoch == 0:
-#     if load_caffe_vgg:
-#         net = vo.OSVOS_VGG(pretrained=2)
-#     else:
-#         net = vo.OSVOS_VGG(pretrained=1)
-# else:
+    def save(self, epoch: int) -> None:
+        pass
+
+
+save_dir = Path('models')
+np = NetworkProvider('vgg16', OSVOS_VGG, save_dir)
+
+# parent train
+resume_epoch = 0
+load_caffe_vgg = False
+if resume_epoch == 0:
+    if load_caffe_vgg:
+        np.init_network(pretrained=2)
+    else:
+        np.init_network(pretrained=1)
+else:
+    np.init_network(pretrained=0)
+    np.load(resume_epoch)
+net = np.network
