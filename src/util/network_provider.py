381a24b465acdf1a081d8db7b7f983ba7486ac0f
OndragKlaus <OndragKlaus@users.noreply.github.com>
2018-02-21 21:39:12 +0100

    added optimizer variants (different optimizers) to resnet offline

--- src/util/network_provider.py
+++ src/util/network_provider.py
@@ -211,6 +211,104 @@ class ResNetOfflineProvider(NetworkProvider):
                 optimizer = optim.SGD(params)
             elif v == 2:
                 optimizer = optim.Adam(params)
+            elif v == 3:
+                optimizer = optim.Adam([
+                    {'params': [pr[1] for pr in net.layer_stages.named_parameters() if 'weight' in pr[0]],
+                     'weight_decay': weight_decay, 'initial_lr': learning_rate},
+                    {'params': [pr[1] for pr in net.layer_stages.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate, 'initial_lr': 2 * learning_rate},
+                    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'weight' in pr[0]],
+                     'weight_decay': weight_decay, 'initial_lr': learning_rate},
+                    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate, 'initial_lr': 2 * learning_rate},
+                    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'weight' in pr[0]],
+                     'lr': learning_rate / 10, 'weight_decay': weight_decay, 'initial_lr': learning_rate / 10},
+                    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate / 10, 'initial_lr': 2 * learning_rate / 10},
+                    {'params': [pr[1] for pr in net.upscale_side_prep.named_parameters() if 'weight' in pr[0]],
+                     'lr': 0, 'initial_lr': 0},
+                    {'params': [pr[1] for pr in net.upscale_score_dsn.named_parameters() if 'weight' in pr[0]],
+                     'lr': 0, 'initial_lr': 0},
+                    {'params': net.layer_fuse.weight, 'weight_decay': weight_decay,
+                     'lr': learning_rate / 100, 'initial_lr': learning_rate / 100},
+                    {'params': net.layer_fuse.bias, 'lr': 2 * learning_rate / 100,
+                     'initial_lr': 2 * learning_rate / 100},
+                ], lr=learning_rate)
+            elif v == 4:
+                optimizer = optim.Adagrad(params)
+            elif v == 5:
+                optimizer = optim.Adagrad([
+                    {'params': [pr[1] for pr in net.layer_stages.named_parameters() if 'weight' in pr[0]],
+                     'weight_decay': weight_decay, 'initial_lr': learning_rate},
+                    {'params': [pr[1] for pr in net.layer_stages.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate, 'initial_lr': 2 * learning_rate},
+                    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'weight' in pr[0]],
+                     'weight_decay': weight_decay, 'initial_lr': learning_rate},
+                    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate, 'initial_lr': 2 * learning_rate},
+                    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'weight' in pr[0]],
+                     'lr': learning_rate / 10, 'weight_decay': weight_decay, 'initial_lr': learning_rate / 10},
+                    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate / 10, 'initial_lr': 2 * learning_rate / 10},
+                    {'params': [pr[1] for pr in net.upscale_side_prep.named_parameters() if 'weight' in pr[0]],
+                     'lr': 0, 'initial_lr': 0},
+                    {'params': [pr[1] for pr in net.upscale_score_dsn.named_parameters() if 'weight' in pr[0]],
+                     'lr': 0, 'initial_lr': 0},
+                    {'params': net.layer_fuse.weight, 'weight_decay': weight_decay,
+                     'lr': learning_rate / 100, 'initial_lr': learning_rate / 100},
+                    {'params': net.layer_fuse.bias, 'lr': 2 * learning_rate / 100,
+                     'initial_lr': 2 * learning_rate / 100},
+                ], lr=learning_rate)
+            elif v == 6:
+                optimizer = optim.Adadelta(params)
+            elif v == 7:
+                optimizer = optim.Adadelta([
+                    {'params': [pr[1] for pr in net.layer_stages.named_parameters() if 'weight' in pr[0]],
+                     'weight_decay': weight_decay, 'initial_lr': learning_rate},
+                    {'params': [pr[1] for pr in net.layer_stages.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate, 'initial_lr': 2 * learning_rate},
+                    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'weight' in pr[0]],
+                     'weight_decay': weight_decay, 'initial_lr': learning_rate},
+                    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate, 'initial_lr': 2 * learning_rate},
+                    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'weight' in pr[0]],
+                     'lr': learning_rate / 10, 'weight_decay': weight_decay, 'initial_lr': learning_rate / 10},
+                    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate / 10, 'initial_lr': 2 * learning_rate / 10},
+                    {'params': [pr[1] for pr in net.upscale_side_prep.named_parameters() if 'weight' in pr[0]],
+                     'lr': 0, 'initial_lr': 0},
+                    {'params': [pr[1] for pr in net.upscale_score_dsn.named_parameters() if 'weight' in pr[0]],
+                     'lr': 0, 'initial_lr': 0},
+                    {'params': net.layer_fuse.weight, 'weight_decay': weight_decay,
+                     'lr': learning_rate / 100, 'initial_lr': learning_rate / 100},
+                    {'params': net.layer_fuse.bias, 'lr': 2 * learning_rate / 100,
+                     'initial_lr': 2 * learning_rate / 100},
+                ], lr=learning_rate)
+            elif v == 8:
+                optimizer = optim.Adamax(params)
+            elif v == 9:
+                optimizer = optim.Adamax([
+                    {'params': [pr[1] for pr in net.layer_stages.named_parameters() if 'weight' in pr[0]],
+                     'weight_decay': weight_decay, 'initial_lr': learning_rate},
+                    {'params': [pr[1] for pr in net.layer_stages.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate, 'initial_lr': 2 * learning_rate},
+                    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'weight' in pr[0]],
+                     'weight_decay': weight_decay, 'initial_lr': learning_rate},
+                    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate, 'initial_lr': 2 * learning_rate},
+                    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'weight' in pr[0]],
+                     'lr': learning_rate / 10, 'weight_decay': weight_decay, 'initial_lr': learning_rate / 10},
+                    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'bias' in pr[0]],
+                     'lr': 2 * learning_rate / 10, 'initial_lr': 2 * learning_rate / 10},
+                    {'params': [pr[1] for pr in net.upscale_side_prep.named_parameters() if 'weight' in pr[0]],
+                     'lr': 0, 'initial_lr': 0},
+                    {'params': [pr[1] for pr in net.upscale_score_dsn.named_parameters() if 'weight' in pr[0]],
+                     'lr': 0, 'initial_lr': 0},
+                    {'params': net.layer_fuse.weight, 'weight_decay': weight_decay,
+                     'lr': learning_rate / 100, 'initial_lr': learning_rate / 100},
+                    {'params': net.layer_fuse.bias, 'lr': 2 * learning_rate / 100,
+                     'initial_lr': 2 * learning_rate / 100},
+                ], lr=learning_rate)
             else:
                 raise ValueError('invalid variant')
         return optimizer
