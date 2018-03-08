ff2d079505676d64a25b0b01f641a8f0d50a8827
OndragKlaus <OndragKlaus@users.noreply.github.com>
2018-03-08 08:31:25 +0100

    added citation and link to github repo

--- src/networks/net_trim.py
+++ src/networks/net_trim.py
@@ -9,7 +9,14 @@ log = get_logger(__file__)
 
 
 class NetTrim(nn.Module):
-    def __init__(self):
+    """
+        Original paper by A. Aghasi, A. Abdi, N. Nguyen, and J. Romberg, "Net-Trim: Convex Pruning of Deep Neural
+        Networks with Performance Guarantee," NIPSs 2017
+        PyTorch implementation of the paper above 18f
+        TensorFlow implementation provided by the authors at https://github.com/DNNToolBox/Net-Trim-v1
+    """
+
+    def __init__(self, rho: float, alpha: float, lmbda: float):
         pass
 
     def forward(self, x):
