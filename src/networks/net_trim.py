d9ca92142d3694cf68328e46254e0d93637f477d
OndragKlaus <OndragKlaus@users.noreply.github.com>
2018-03-08 08:04:26 +0100

    added net trim

---
+++ src/networks/net_trim.py
@@ -0,0 +1,16 @@
+from typing import List, Tuple, Union, Callable
+
+import torch
+import torch.nn as nn
+
+from util.logger import get_logger
+
+log = get_logger(__file__)
+
+
+class NetTrim(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
