b83a6e573b88a2ffcc05a5e4eceb068db2fa7eda
OndragKlaus <OndragKlaus@users.noreply.github.com>
2018-07-11 01:11:20 +0200

    added method stub for nn

--- src/run_webcam.py
+++ src/run_webcam.py
@@ -7,20 +7,23 @@ via the new Pythonic cv2 interface.  Press <esc> to quit.
 import cv2
 
 
-def show_webcam(mirror=False):
+def show_webcam(net, mirror=False):
     cam = cv2.VideoCapture(0)
     while True:
         ret_val, img = cam.read()
         if mirror: 
             img = cv2.flip(img, 1)
+        img = apply_network(img, net)
         cv2.imshow('my webcam', img)
         if cv2.waitKey(1) == 27: 
             break  # esc to quit
     cv2.destroyAllWindows()
 
+def apply_network(img, net):
+    return img
 
 def main():
-    show_webcam(mirror=True)
+    show_webcam(None, mirror=True)
 
 
 if __name__ == '__main__':
