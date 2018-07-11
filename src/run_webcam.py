fdfb0c6e4f63a4ef952e47e5799e43558d550f81
OndragKlaus <OndragKlaus@users.noreply.github.com>
2018-07-11 02:12:22 +0200

    added argument to disable cuda

--- src/run_webcam.py
+++ src/run_webcam.py
@@ -9,7 +9,6 @@ import click
 import numpy as np
 import cv2
 import torch
-from torch.autograd import Variable
 
 from networks.osvos_resnet import OSVOS_RESNET
 from networks.osvos_vgg import OSVOS_VGG
@@ -23,13 +22,16 @@ mean_value = np.array((104.00699, 116.66877, 122.67892), dtype=np.float32)
 @click.option('--webcam', type=int, default=0)
 @click.option('--mirror/--no-mirror', default=True)
 @click.option('--use-network/--no-use-network', default=True)
-def main(variant: str, webcam: int, mirror: bool, use_network: bool) -> None:
+@click.option('--use-cuda/--no-use-cuda', default=True)
+def main(variant: str, webcam: int, mirror: bool, use_network: bool, use_cuda: bool) -> None:
     if use_network:
         net = get_network(variant)
+        if use_cuda:
+            net = net.cuda()
     else:
         net = None
     cam = cv2.VideoCapture(webcam)
-    loop_video(net, cam, mirror)
+    loop_video(net, cam, mirror, use_cuda)
     cv2.destroyAllWindows()
 
 
@@ -48,39 +50,40 @@ def get_network(variant: str) -> torch.nn.Module:
         raise Exception('Not yet implemented')
     else:
         raise Exception('Click should have prevented this')
-    net = net.cuda()
     return net
 
 
-def loop_video(net: Optional[torch.nn.Module], cam: cv2.VideoCapture, mirror: bool) -> None:
+def loop_video(net: Optional[torch.nn.Module], cam: cv2.VideoCapture, mirror: bool, use_cuda: bool) -> None:
     use_network = net is not None
     while True:
         ret_val, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
         if use_network:
-            img = apply_network(net, img)
+            img = apply_network(net, img, use_cuda)
         cv2.imshow('my webcam', img)
         if cv2.waitKey(1) == 27:
             break  # esc to quit
 
 
-def apply_network(net: torch.nn.Module, img: np.ndarray) -> np.ndarray:
+def apply_network(net: torch.nn.Module, img: np.ndarray, use_cuda: bool) -> np.ndarray:
     img = img - mean_value
     img = to_tensor(img)
+    if use_cuda:
+        img = img.cuda()
     outputs = net.forward(img)
     output = outputs[-1]
     output = to_numpy(output)
     return output
 
 
-def to_tensor(img: np.ndarray) -> torch.nn.Module:
+def to_tensor(img: np.ndarray) -> torch.autograd.Variable:
     img = img[np.newaxis, ...]
     img = torch.from_numpy(img.transpose((0, 3, 1, 2)))
     if isinstance(img, torch.ByteTensor):
         img = img.float().div(255)
-    img = Variable(img, volatile=True)
     img = img.cuda()
+    img = torch.autograd.Variable(img, volatile=True)
     return img
 
 
